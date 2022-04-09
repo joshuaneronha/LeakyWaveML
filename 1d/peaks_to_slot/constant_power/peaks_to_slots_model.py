afc1bfb184347dc8ccb5a712efbae83794a0c4f3
joshuaneronha <77899869+joshuaneronha@users.noreply.github.com>
2022-04-09 19:05:16 -0400

    prewitt filter

--- 1d/peaks_to_slot/constant_power/peaks_to_slots_model.py
+++ 1d/peaks_to_slot/constant_power/peaks_to_slots_model.py
@@ -69,18 +69,23 @@ class LWAPredictionModel(tf.keras.Model):
 
         # print(prediction)
         bce = tf.keras.losses.BinaryCrossentropy()
+        bce_prewitt = tf.keras.losses.BinaryCrossentropy()
         bce_laplacian = tf.keras.losses.BinaryCrossentropy()
         # bce_exp2 = tf.keras.losses.BinaryCrossentropy()
 
+        true_prewitt = tfio.experimental.filter.prewitt(tf.cast(tf.reshape(true, [-1,1,36,1]),tf.float32))
+        pred_prewitt = tfio.experimental.filter.prewitt(tf.round(tf.cast(tf.reshape(prediction, [-1,1,36,1]),tf.float32)))
+
         true_laplacian = tfio.experimental.filter.laplacian(tf.cast(tf.reshape(true, [-1,1,36,1]),tf.float32), ksize = [1,3])
         pred_laplacian = tfio.experimental.filter.laplacian(tf.round(tf.cast(tf.reshape(prediction, [-1,1,36,1]),tf.float32)), ksize = [1,3])
+
 
 
         # tf_data.shape
         # plt.imshow(tf.reshape(tfio.experimental.filter.laplacian(tf.cast(tf_data,tf.float32), ksize=[1,3]),[36,1]),cmap='YlGnBu')
 
         # return bce(true,prediction) + 0.5*bce_exp(true_pooled, pred_pooled) + (0*bce_exp2(true_pooled_2, pred_pooled_2))
-        return bce(true,prediction) + bce_laplacian(true_laplacian, pred_laplacian)
+        return bce(true,prediction) + bce_prewitt(true_prewitt, pred_prewitt) + bce_laplacian(true_laplacian, pred_laplacian)
 
     def accuracy(self, prediction, true):
         ba = tf.keras.metrics.BinaryAccuracy()
